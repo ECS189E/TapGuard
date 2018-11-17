cc51a89ef945956f277f33ebab0f742dbc27c239
Xian Dan Huang <xiandanhuang@MacBook-Pro-2.local>
2018-11-17 07:51:34 -0800

    [PERMISSIONS] Fixed permissions

--- TapGuard/TapGuard/View Controllers/AppDelegate.swift
+++ TapGuard/TapGuard/View Controllers/AppDelegate.swift
@@ -84,7 +84,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
