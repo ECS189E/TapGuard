c42ed74962c35bf4b509819b71adea4587a67028
Xian Dan Huang <xiandanhuang@MacBook-Pro-2.attlocal.net>
2018-11-18 10:00:04 -0800

    [MISC] Update state before push to master

--- TapGuard/TapGuard/View Controllers/AppDelegate.swift
+++ TapGuard/TapGuard/View Controllers/AppDelegate.swift
@@ -13,8 +13,6 @@ import GoogleSignIn
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
-    var window: UIWindow?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
