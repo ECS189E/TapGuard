e67a84f78fc6a945b167bd39798885c494ac4d72
charlesgoh <charles.goh@live.com>
2018-11-20 10:26:18 -0800

    [FIX][FUNCTIONS] Fixed retrieving and storing of data to database functionality

--- TapGuard/TapGuard/Models/Functions.swift
+++ TapGuard/TapGuard/Models/Functions.swift
@@ -37,13 +37,13 @@ struct Functions{
         })
     }
     
-    static func setupUserInDatabase(user: User) {
-        let userRef = Database.database().reference().child("users").child(user.userId)
-        userRef.child("username").setValue(user.userName)
-        userRef.child("phoneString").setValue(user.phoneNumber)
-        userRef.child("email").setValue(user.email)
-        userRef.child("verified").setValue((user.verified ? "true":"false"))
-    }
+//    static func setupUserInDatabase(user: User) {
+//        let userRef = Database.database().reference().child("users").child(user.userId)
+//        userRef.child("username").setValue(user.userName)
+//        userRef.child("phoneString").setValue(user.phoneNumber)
+//        userRef.child("email").setValue(user.email)
+//        userRef.child("verified").setValue((user.verified ? "true":"false"))
+//    }
     
     // Attempt to get user. Otherwise create a dummy user with a verified check value as false
     static func getUserFromDatabase(user: GIDGoogleUser, completion: @escaping (User) -> Void) {
@@ -72,17 +72,42 @@ struct Functions{
                     contactsArray.append(emergencyContactInstance)
                 }
                 
-                // Construct User
+                // Construct User and return to user to caller
                 let userInstance = User(userId: user.userID, userName: userName, email: email, phoneNumber: phoneNumberString, verified: isVerifiedString, contacts: contactsArray)
                 completion(userInstance)
             } else {
                 // Construct new user using information we already have from google account
-                completion(User(userId: user.userID, userName: user.profile.name, email: user.profile.email, phoneNumber: "", verified: false, contacts: []))
+                // and save it in database, then return user to caller
+                let userInstance = User(userId: user.userID, userName: user.profile.name, email: user.profile.email, phoneNumber: "", verified: false, contacts: [])
+                updateUserDetails(user: userInstance, completion: { (isSuccessful) in
+                    if isSuccessful {
+                        print("Updating is successful")
+                    } else {
+                        print("Updating is not successful")
+                    }
+                })
+                completion(userInstance)
             }
         }
     }
     
-    static func updateUserDetails(user: User, completion: @escaping (User) -> Void) {
-        // Updates user details including emergency contacts
+    // Updates user details
+    static func updateUserDetails(user: User, completion: @escaping (Bool) -> Void) {
+        let userRef = Database.database().reference().child("users").child(user.userId)
+        userRef.child("username").setValue(user.userName)
+        userRef.child("phoneString").setValue(user.phoneNumber)
+        userRef.child("email").setValue(user.email)
+        userRef.child("verified").setValue((user.verified))
+        
+        // Parse contacts data and pass to database
+        var contactsArray : [NSDictionary] = []
+        for contact in user.contacts {
+            let contactDictionary : NSDictionary = ["userName": contact.userName, "phoneNumberString": contact.phoneNumber, "isTrusted": contact.isTrusted, "isLocationSharingOn": contact.isLocationSharingOn, "isPrimary": contact.isPrimary]
+            contactsArray.append(contactDictionary)
+        }
+        userRef.child("contacts").setValue(contactsArray)
+        
+        // Successfully updated: Return true
+        completion(true)
     }
 }
