14106c7708b661ed7a9c515a69cc5c339d8c980f
Saksham Bhalla <iambhalla@ucdavis.edu>
2018-11-28 19:25:08 -0800

    [HOMEVC] added outlets, initialized functions

--- TapGuard/TapGuard/View Controllers/HomeViewController.swift
+++ TapGuard/TapGuard/View Controllers/HomeViewController.swift
@@ -9,18 +9,49 @@
 import UIKit
 import LocationPickerViewController
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITextFieldDelegate {
     
     var user : User?
    
+    @IBOutlet weak var destinationTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.destinationTextField.delegate = self
         self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         NotificationCenter.default.post(name: .didRetreatFromHome, object: self, userInfo: ["Success": 1])
     }
+    
+    @IBAction func bikePressed(_ sender: Any) {
+        print("bike pressed")
+    }
+    
+    @IBAction func carPressed(_ sender: Any) {
+        print("car pressed")
+    }
+    
+    @IBAction func walkPressed(_ sender: Any) {
+        print("walk pressed")
+    }
+    
+    
+    @IBAction func startJourneyPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func logoutButtonPressed(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func settingsButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "presentSettingsFromHome", sender: self)
+    }
+}
+
+
+
 //
 //    @IBAction func selectDestinationPressed(_ sender: Any) {
 //        let locationPicker = LocationPicker()
@@ -29,20 +60,9 @@ class HomeViewController: UIViewController {
 //        }
 //        locationPicker.addBarButtons()
 //        // Call this method to add a done and a cancel button to navigation bar.
-//        
+//
 //        let navigationController = UINavigationController(rootViewController: locationPicker)
 //        present(navigationController, animated: true, completion: nil)
 //    }
-//    
-//    @IBAction func startJourneyButtonPressed(_ sender: Any) {
-//        print(countDownTimer.minuteInterval.magnitude)
-//    }
-//    
-//    @IBAction func logoutButtonPressed(_ sender: Any) {
-//        navigationController?.popViewController(animated: true)
-//    }
 //
-//    @IBAction func settingsButtonPressed(_ sender: Any) {
-//        performSegue(withIdentifier: "presentSettingsFromHome", sender: self)
-//    }
-}
+//
