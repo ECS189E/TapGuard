44a481de4f3bfc236e91f95fbe632d70f249c2ff
Saksham Bhalla <iambhalla@ucdavis.edu>
2018-11-28 19:07:04 -0800

    [HOMEVC] updated UI

--- TapGuard/TapGuard/View Controllers/HomeViewController.swift
+++ TapGuard/TapGuard/View Controllers/HomeViewController.swift
@@ -12,43 +12,37 @@ import LocationPickerViewController
 class HomeViewController: UIViewController {
     
     var user : User?
-    @IBOutlet weak var countDownTimer: UIDatePicker!
-    
-    @IBOutlet weak var selectedDestinationLabel: UILabel!
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.setNavigationBarHidden(true
-            , animated: true)
-        selectedDestinationLabel.text = "Please enter Destination"
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
-        // When logging out. Notify system to log out of google auth
         NotificationCenter.default.post(name: .didRetreatFromHome, object: self, userInfo: ["Success": 1])
     }
-    
-    @IBAction func selectDestinationPressed(_ sender: Any) {
-        let locationPicker = LocationPicker()
-        locationPicker.pickCompletion = { (pickedLocationItem) in
-            self.selectedDestinationLabel.text = pickedLocationItem.formattedAddressString ?? "Error getting destination"
-        }
-        locationPicker.addBarButtons()
-        // Call this method to add a done and a cancel button to navigation bar.
-        
-        let navigationController = UINavigationController(rootViewController: locationPicker)
-        present(navigationController, animated: true, completion: nil)
-    }
-    
-    @IBAction func startJourneyButtonPressed(_ sender: Any) {
-        print(countDownTimer.minuteInterval.magnitude)
-    }
-    
-    @IBAction func logoutButtonPressed(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
-    }
-
-    @IBAction func settingsButtonPressed(_ sender: Any) {
-        performSegue(withIdentifier: "presentSettingsFromHome", sender: self)
-    }
+//
+//    @IBAction func selectDestinationPressed(_ sender: Any) {
+//        let locationPicker = LocationPicker()
+//        locationPicker.pickCompletion = { (pickedLocationItem) in
+//            self.selectedDestinationLabel.text = pickedLocationItem.formattedAddressString ?? "Error getting destination"
+//        }
+//        locationPicker.addBarButtons()
+//        // Call this method to add a done and a cancel button to navigation bar.
+//        
+//        let navigationController = UINavigationController(rootViewController: locationPicker)
+//        present(navigationController, animated: true, completion: nil)
+//    }
+//    
+//    @IBAction func startJourneyButtonPressed(_ sender: Any) {
+//        print(countDownTimer.minuteInterval.magnitude)
+//    }
+//    
+//    @IBAction func logoutButtonPressed(_ sender: Any) {
+//        navigationController?.popViewController(animated: true)
+//    }
+//
+//    @IBAction func settingsButtonPressed(_ sender: Any) {
+//        performSegue(withIdentifier: "presentSettingsFromHome", sender: self)
+//    }
 }
